26af3b6d2d6d35d4e3ac3d8d127a0ca9762b72d5
HeDichoQueNo <no@nomail.no>
2025-08-09 19:38:49 +0200

    Se añade fecha de entrada y salida ademas de hashcode equals y toString

--- src/main/java/es/cic/curso25/proy015/Model/Multa.java
+++ src/main/java/es/cic/curso25/proy015/Model/Multa.java
@@ -1,30 +1,87 @@
 package es.cic.curso25.proy015.Model;
 
+import java.time.LocalDate;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.PastOrPresent;
 
 @Entity
-@Table(name="multa")
+@Table(name = "multa")
 public class Multa {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column(name="precio")
-    public static final int PRECIO=5;
+    @Column(name = "precio")
+    public static final int PRECIO = 5;
+
+    @PastOrPresent
+    @Column(name = "fecha_entrada")
+    private LocalDate fechaEntrada;
+
+    @Column(name = "fecha_salida")
+    private LocalDate fechaSalida;
 
     public Long getId() {
         return id;
+    }
+
+    public LocalDate getFechaEntrada() {
+        return fechaEntrada;
+    }
+
+    public void setFechaEntrada(LocalDate fechaEntrada) {
+        this.fechaEntrada = fechaEntrada;
+    }
+
+    public LocalDate getFechaSalida() {
+        return fechaSalida;
+    }
+
+    public void setFechaSalida(LocalDate fechaSalida) {
+        this.fechaSalida = fechaSalida;
     }
 
     public void setId(Long id) {
         this.id = id;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Multa other = (Multa) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Multa [id=" + id + ", fechaEntrada=" + fechaEntrada + ", fechaSalida=" + fechaSalida + "]";
+    }
+
     
+
 }
